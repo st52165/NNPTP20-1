f415d6a328f15e6e6a6c2582867a3f96bc298336
st52165 <fanda.silar@seznam.cz>
2020-12-08 18:51:50 +0100

    Class CplxTests added back to the solution.
    Renamed and refactored the CplxTests class.

---
+++ INPTPZ1Tests/ComplexNumberTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using INPTPZ1.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace INPTPZ1.Mathematics.Tests
+{
+    [TestClass()]
+    public class ComplexNumberTests
+    {
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Complex summand = new Complex(10, 20);
+            Complex addend = new Complex(1, 2);
+
+            Complex total = summand + addend;
+
+            Complex expectedResult = new Complex(11, 22);
+
+            Assert.AreEqual(expectedResult, total);
+        }
+    }
+}
+
+
